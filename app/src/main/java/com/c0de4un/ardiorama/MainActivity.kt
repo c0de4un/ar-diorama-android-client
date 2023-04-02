ec017c8ac76f18c75289c9ebdfab46adb6dc9033
c0de4un <code4un@yandex.ru>
2023-04-02 21:35:29 +0400

    Add open Camera logic

--- app/src/main/java/com/c0de4un/ardiorama/MainActivity.kt
+++ app/src/main/java/com/c0de4un/ardiorama/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.c0de4un.ardiorama
 
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
+import android.graphics.SurfaceTexture
+import android.hardware.camera2.CameraDevice
+import android.hardware.camera2.CameraManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.view.Surface
+import android.view.TextureView
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import com.c0de4un.ardiorama.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var cameraDevice: CameraDevice
+    private lateinit var handler: Handler
     private lateinit var binding: ActivityMainBinding
+    private lateinit var textureView: TextureView
+    private lateinit var cameraManager: CameraManager
 
     private val cameraPermissionCode = 101
 
@@ -21,8 +33,39 @@ class MainActivity : AppCompatActivity() {
 
         getPermission()
 
+        val handlerThread = HandlerThread("videoThread")
+        handler = Handler(handlerThread.looper)
+
+        cameraManager = getSystemService(CAMERA_SERVICE) as CameraManager
+
+        textureView = findViewById(R.id.textureView)
+        textureView.surfaceTextureListener = object:TextureView.SurfaceTextureListener {
+            override fun onSurfaceTextureAvailable(
+                surface: SurfaceTexture,
+                width: Int,
+                height: Int
+            ) {
+                openCamera()
+            }
+
+            override fun onSurfaceTextureSizeChanged(
+                surface: SurfaceTexture,
+                width: Int,
+                height: Int
+            ) {
+            }
+
+            override fun onSurfaceTextureDestroyed(surface: SurfaceTexture): Boolean {
+                return false
+            }
+
+            override fun onSurfaceTextureUpdated(surface: SurfaceTexture) {
+            }
+
+        }
+
         // Example of a call to a native method
-        binding.sampleText.text = stringFromJNI()
+        //binding.sampleText.text = stringFromJNI()
     }
 
     /**
@@ -43,6 +86,28 @@ class MainActivity : AppCompatActivity() {
         if ( ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED ) {
             requestPermissions(arrayOf(android.Manifest.permission.CAMERA), cameraPermissionCode)
         }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun openCamera() {
+        cameraManager.openCamera(
+            cameraManager.cameraIdList[0],
+            object:CameraDevice.StateCallback() {
+                override fun onOpened(camera: CameraDevice) {
+                    cameraDevice = camera
+
+                    val surfaceTexture = textureView.surfaceTexture
+                    val surface = Surface(surfaceTexture)
+                }
+
+                override fun onDisconnected(camera: CameraDevice) {
+                }
+
+                override fun onError(camera: CameraDevice, error: Int) {
+                }
+            },
+            handler
+        )
     }
 
     override fun onRequestPermissionsResult(
