e14064eae51719b8eef1aa955e0958111a40be2c
c0de4un <code4un@yandex.ru>
2023-04-02 20:55:07 +0400

    Add Camera permission request

--- app/src/main/java/com/c0de4un/ardiorama/MainActivity.kt
+++ app/src/main/java/com/c0de4un/ardiorama/MainActivity.kt
@@ -1,19 +1,25 @@
 package com.c0de4un.ardiorama
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import com.c0de4un.ardiorama.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val cameraPermissionCode = 101
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        getPermission()
 
         // Example of a call to a native method
         binding.sampleText.text = stringFromJNI()
@@ -29,6 +35,27 @@ class MainActivity : AppCompatActivity() {
         // Used to load the 'ardiorama' library on application startup.
         init {
             System.loadLibrary("ardiorama")
+        }
+    }
+
+    private fun getPermission()
+    {
+        if ( ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED ) {
+            requestPermissions(arrayOf(android.Manifest.permission.CAMERA), cameraPermissionCode)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == cameraPermissionCode) {
+            if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                getPermission()
+            }
         }
     }
 }
